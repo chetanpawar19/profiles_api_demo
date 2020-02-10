df6c98e6df1c472f97a21a33c5e70906a65dbf82
chetanpawar <chetanpawar19@outlook.com>
2020-02-11 00:11:09 +0530

    permissions added

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,7 +1,34 @@
 from rest_framework import serializers
+from profiles_api import models
 
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing APIView"""
 
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializers a user profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields=('id','email','name','password')
+        extra_kwargs={           #for customization, below field only for write, not read
+            'password':{
+                'write_only':True,
+                'style':{'input_type':'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Create & return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+        return user
+
+
+
